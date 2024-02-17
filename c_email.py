c93e880e78b3d28f5c57085f430f4db7f7db1bf8
donbassik1228 <148227783+donbassik1228@users.noreply.github.com>
2024-02-17 23:25:18 +0200

    create func in class

--- c_email.py
+++ c_email.py
@@ -1,5 +1,23 @@
-﻿class Email():
-    def __init__(self) -> None:
-        pass
+﻿class Email:
+    def __init__(self):
+        self.emails = []
+    def add_email(self, email):
+        if email in self.emails:
+            raise ValueError("This email address already exists.")
+        self.emails.append(email)
 
-# gjkhgdhdljhgipdngpd
+    def edit_email(self, old_email, new_email):
+        if old_email not in self.emails:
+            raise ValueError("This email address was not found.")
+        if new_email in self.emails:
+            raise ValueError("The new email address already exists.")
+        index = self.emails.index(old_email)
+        self.emails[index] = new_email
+
+    def remove_email(self, email):
+        if email not in self.emails:
+            raise ValueError("This email address was not found")
+        self.emails.remove(email)
+    
+
+
