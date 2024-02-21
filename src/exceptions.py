19ca330a69530fa46937a51a9793c3a1f67235d0
w00her <rodon@protonmail.ch>
2024-02-21 21:31:59 +0200

    fixed space

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,4 @@
-﻿﻿class PhoneNotFound(Exception):
+﻿class PhoneNotFound(Exception):
     def __init__(self, message='Number not found'):
         self.message = message
         super().__init__(self.message)
